3592bcbaeca433b6f7e3705b5bff64279171fe30
Patrick-Santana <5171535@alunouninter.com>
2026-09-24 12:03:32 -0300

    [ GH-04 ] - Implementação do model de Relatorio

---
+++ models/relatorio.py
@@ -0,0 +1,24 @@
+# models/relatorio.py
+from dataclasses import dataclass, field
+from typing import List, Dict
+from models.fiscalizacao import Fiscalizacao
+from models.nao_conformidade import NaoConformidade
+
+@dataclass
+class Relatorio:
+    fiscalizacao: Fiscalizacao
+    nao_conformidades: List[NaoConformidade] = field(default_factory=list)
+    
+    # Dados para Relatório de Qualidade
+    indicadores: Dict = field(default_factory=dict)
+    tabelas_extrapolacao: Dict = field(default_factory=dict)
+    
+    # Dados para Relatório de Acompanhamento
+    linha_do_tempo: List[Dict] = field(default_factory=list)
+    planos_acao: List[Dict] = field(default_factory=list)
+    
+    # Apêndices fotográficos
+    tem_condicoes_gerais: bool = False
+    fotos_condicoes_gerais: List[str] = field(default_factory=list)
+    
+    output_path: str = ""
