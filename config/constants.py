9c4a5f058e7e9b9a8455df5bfcfd0c693944e02b
Patrick-Santana <5171535@alunouninter.com>
2026-09-24 12:04:57 -0300

    [ GH-02 ] - Constantes utilizadas nos Relatórios

--- config/constants.py
+++ config/constants.py
@@ -1,16 +1,53 @@
 # config/constants.py
+
+# Tipos de Relatórios ARPE suportados
+TIPOS_RELATORIO = {
+    'QUALIDADE_TRIMESTRAL': '📊 Relatório de Qualidade (Trimestral)',
+    'QUALIDADE_SEMESTRAL': '📈 Relatório de Qualidade (Semestral)',
+    'ACOMPANHAMENTO': '📋 Relatório de Acompanhamento (Comunicação)',
+    'DIRETA': '🔍 Relatório de Fiscalização Direta',
+}
+
+# Concessionárias
+CONCESSIONARIAS = {
+    'CELPE': 'Companhia Energética de Pernambuco (Celpe)',
+    'COPERGAS': 'Companhia Pernambucana de Gás (Copergás)'
+}
+
+# Incisos da Resolução ARPE nº 034/2006
+INCISOS_RESOLUCAO = {
+    'I': 'Prazo Máximo para Atendimento a Pedido de Ligação',
+    'II': 'Prazo Máximo para Atendimento a Pedido de Religação',
+    'III': 'Prazo Máximo para Religação de Usuário com Corte Indevido',
+    'IV': 'Tempo Máximo de Interrupção para Manutenção Programada',
+    'V': 'Prazo para Devolução de Valores por Erros de Faturamento',
+    'VI': 'Prazo Máximo para Troca de Medidor',
+    'VII': 'Prazo Máximo para Verificação de Pressão e/ou PCS do Gás',
+    'VIII': 'Tempo Médio de Elaboração de Estudos e Orçamentos'
+}
+
+# Colunas das planilhas
 COL_FISCALIZACOES = {
     'ID': 'ID',
     'MUNICIPIO': 'Município',
-    'TIPO': 'Tipo',               # valor esperado: "Celpe/Copergás" (ou "Energia e Gás")
+    'TIPO': 'Tipo',
+    'PERIODO': 'Período',
+    'PROCESSO_ADM': 'Processo Administrativo',
     'DATA': 'Data',
     'RELATORIO_GERADO': 'Relatório Gerado'
 }
 
 COL_NAO_CONFORMIDADES = {
     'ID_FISCALIZACAO': 'ID Fiscalização',
+    'INCISO': 'Inciso',
+    'INDICADOR': 'Indicador',
     'DESCRICAO': 'Descrição',
-    'NOME_FOTO': 'Nome da Foto'
+    'FUNDAMENTACAO': 'Fundamentação',
+    'QUANTIDADE': 'Quantidade',
+    'NOME_FOTO': 'Nome da Foto',
+    'JUSTIFICATIVA': 'Justificativa Concessionária',
+    'STATUS': 'Status Acatamento',
+    'PLANO_ACAO': 'Plano de Ação'
 }
 
 COL_UNIDADES = {
@@ -18,26 +55,15 @@ COL_UNIDADES = {
     'UNIDADE': 'Unidade'
 }
 
-COL_ENVIO_DOCS = {
-    'ID_FISCALIZACAO': 'ID Fiscalização',
-    'DOCUMENTO': 'Documento',
-    'STATUS': 'Status'
-}
-
-COL_COMPESA = {   # mantido nome original da planilha Copergás
-    'DT_SOLICITACAO': 'Dt Solicitação RA',
-    'DT_ENCERRAMENTO': 'Dt Encerramento RA',
-    'PRAZO_TIPO': 'Prazo Tipo Sol RA',
-    'MOTIVO_ENCERRA': 'Motivo Encer RA'
-}
-
-TEMPLATE_PLACEHOLDERS = {
+# Placeholders dos templates
+PLACEHOLDERS = {
     'ID': '{{ID}}',
     'MUNICIPIO': '{{MUNICIPIO}}',
     'DATA': '{{DATA}}',
-    'TIPO': '{{TIPO}}',
-    'UNIDADE': '{{UNIDADE}}',
-    'NUM_NCS': '{{NUM_NCS}}',
+    'PERIODO': '{{PERIODO}}',
+    'PROCESSO_ADM': '{{PROCESSO_ADM}}',
+    'CONCESSIONARIA': '{{CONCESSIONARIA}}',
+    'TOTAL_NCS': '{{TOTAL_NCS}}',
     'DT_SOLICITACAO': '{{DT_SOLICITACAO}}',
     'DT_ENCERRAMENTO': '{{DT_ENCERRAMENTO}}',
     'PRAZO_TIPO': '{{PRAZO_TIPO}}',
